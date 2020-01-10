5e1b239b12bbc451458cd61133bc4e3eedbffb75
MrCallum <mrcallummorrison@gmail.com>
2020-01-10 14:20:37 +0000

    Content changes and nav li animation

--- Resources/JS/main.js
+++ Resources/JS/main.js
@@ -1,18 +1,10 @@
 const context = {
     likes : [
         {
-            mainText : "Favourite fruit",
-            commentText : "Apples",
-            hasImage : true,
-            imgSource = "https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/health-benefits-of-apples-1296x728-feature.jpg?w=1155&h=1528",
-        }
-    ],
-    dislikes : [
-        {
-            mainText : "Unpopular opinion",
-            commentText : "Even though TV quality is higher than ever, I think ",
-            hasImage : true,
-            imgSource = "https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/health-benefits-of-apples-1296x728-feature.jpg?w=1155&h=1528",
+            mainText : "Favourite Paul Thomas Anderson Film",
+            commentText : "Phantom Thread",
+            imgSource : "https://thefilmstage.com/wp-content/uploads/2017/12/Phantom-Thread-header.jpg",
+            link : "https://www.imdb.com/title/tt5776858/"
         }
     ]
 }
@@ -22,3 +14,4 @@ const templateSource = templateElement.innerHTML;
 const template = Handlebars.compile(templateSource); // After calling Handlebars.compile() with an argument, a function is returned to the template. 
 const compiledHtml = template(context); // template will accept an object and use the properties of the object to fill in a Handlebars template.
 document.getElementById('information').innerHTML = compiledHtml;
+
