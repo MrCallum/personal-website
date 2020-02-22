1db193d9daa2d92796ee6a8a1efa8ab60845aac1
MrCallum <mrcallummorrison@gmail.com>
2020-02-22 17:46:50 +0000

    Basic hamburger menu working (too small)

--- Resources/JavaScript/MobileMenu.js
+++ Resources/JavaScript/MobileMenu.js
@@ -1,3 +1,7 @@
+const mobMenu = document.getElementById("mobile-menu");
+const hamburgerButton = document.getElementById("hamburger-button");
+
+
 function openMobileMenu(){
     document.getElementById("mobile-menu").style.width = "100%";
 }
@@ -6,5 +10,16 @@ function closeMobileMenu(){
     document.getElementById("mobile-menu").style.width = "0%";
 }
 
+function toggleMobileMenu(){
+    const computedStyle = window.getComputedStyle(mobMenu, null);
+
+    if(computedStyle.width !== "0px"){
+        mobMenu.style.width = "0%";
+        hamburgerButton.classList.remove("is-active");
+    } else {
+        mobMenu.style.width = "100%";
+        hamburgerButton.classList.add("is-active");
+    }
+}
 
 
